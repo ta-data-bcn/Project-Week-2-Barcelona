e1d284c8a9bdd57cebc780f75db2f0775ecb2097
Oriol Cunill <cunillati@gmail.com>
2020-01-23 11:19:12 +0100

    file to import data from CSV and create the inserts

--- import_data_from_csv.py
+++ import_data_from_csv.py
@@ -2,7 +2,7 @@ import pymysql
 from pathlib import Path
 import pandas as pd
 
-credentials = Path('./connection/credentials.txt')
+credentials = Path('./Project-Week-2-Barcelona/connection/credentials.txt')
 if (not credentials.exists()):
     print('ERROR - COULD NOT RECOVER CREDENTIAL FILES')
 
@@ -10,15 +10,14 @@ with open(credentials) as f:
     lines = f.read().splitlines()
     f.close()
 
-data_folder = Path('./datasets/2.-Urban-Environment/air-stations-nov-2017.csv')
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/air-stations-nov-2017.csv')
 df = pd.read_csv(data_folder)
 #print(df.columns)
 print(df)
 
 # create INSERT sentence
-insert_sql = 'INSERT INTO Station ({}) VALUES (%s,%s,%s)'
+insert_sql = 'INSERT INTO Station ({}) VALUES (%s,%f,%f)'
 column_names = 'name, lat, lng'
-
 
 conn = pymysql.connect(host=lines[0], user=lines[1], password=lines[2], db=lines[3])
 
@@ -26,10 +25,14 @@ conn = pymysql.connect(host=lines[0], user=lines[1], password=lines[2], db=lines
 cursor = conn.cursor()
 
 for i,row in df.iterrows():
-    cursor.execute(insert_sql.format(column_names), [row[0], float(row[1]), float(row[2])])
+    try:
+        cursor.execute(insert_sql.format(column_names), [row[0], float(row[1]), float(row[2])])
+        conn.commit()
+    except:
+        conn.rollback()
+        print(f'ERROR - could not insert querie {insert_sql.format(column_names)} with params {[row[0], float(row[1]), float(row[2])]}')
 
 # the connection is not autocommitted by default, so we must commit to save our changes
-conn.commit()
 
 conn.close()
 cursor.close()
