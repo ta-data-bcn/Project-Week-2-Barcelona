243715bc25f3fc245bb8f0632772ff6147c5b0d2
Oriol Cunill <cunillati@gmail.com>
2020-01-23 18:04:33 +0100

    add inserts from 2018 and first quarter 2019

--- your-project/connector.py
+++ your-project/connector.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pandas as pd
 import sys
 from datetime import datetime
+import re
 
 class DBConnector:
     def __init__(self, path):
@@ -42,9 +43,13 @@ class DBConnector:
 
 def recover_integer(val):
     try:
-        new_val = int(val)
+        new_val = int(val.values[0])
     except:
-        new_val = -1
+        try:
+            tmp = re.subn('[^\d]','',str(val))
+            new_val = int(tmp[0])
+        except:
+            new_val = -1
     return new_val
 
 def recover_date(val):
@@ -55,7 +60,28 @@ def recover_date(val):
         generated = datetime.now()
     return generated.strftime('%Y-%m-%d %H:%M:%S')
 
+def run_csv_file_into_sql(registry_df, stations):
+    # setting values to create the inserts
+    table_name = 'Registry'
+    column_names = 'no2_Val, o3_Val, pm_val, date_generate, station_ID'
+    values = '%s,%s,%s,%s,%s'
 
+    for i,row in registry_df.iterrows():
+        station = stations.query(f'name == \'{row["Station"].strip()}\'')
+        if (station.size == 0):
+            print('no station found with name ',row["Station"])
+            # print('\n\nWARNING - row not saved \n',row)
+            continue
+        station_ID = recover_integer(station.station_ID)
+        if (station_ID == -1):
+            print('no station saved with name ',row["Station"])
+            continue
+        no2_value = recover_integer(row['NO2 Value'])
+        o3_value = recover_integer(row['O3 Value'])
+        pm10_value = recover_integer(row['PM10 Value'])
+        date_generated = recover_date(str(row.Generated))
+        row_values = [no2_value, o3_value, pm10_value, date_generated, int(station_ID)]
+        db_conn.insert_values(table_name, column_names, values, row_values)
 
 
 
@@ -74,39 +100,64 @@ column_names = 'name, lat, lng'
 values = '%s,%s,%s'
 
 for i,row in station_df.iterrows():
-    db_conn.insert_values(table_name, column_names, values, [row[0], row[1], row[2]])
+    db_conn.insert_values(table_name, column_names, values, [row[0].strip(), row[1], row[2]])
 
 
 ########################
 # REGISTRY #############
 ########################
-data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/air-quality-nov-2017.csv')
-registry_df = pd.read_csv(data_folder)
-print('\nimported data -->\n', registry_df)
-# setting values to create the inserts
-table_name = 'Registry'
-column_names = 'no2_Val, o3_Val, pm_val, date_generate, station_ID'
-values = '%s,%s,%s,%s,%s'
-
 # get station IDs
 stations = db_conn.execute_query('SELECT ID as station_ID, name FROM Station')
 
-for i,row in registry_df.iterrows():
-    station = stations.query(f'name == \'{row["Station"].strip()}\'')
-    if (station.size == 0):
-        print('no station found with name ',row["Station"])
-        # print('\n\nWARNING - row not saved \n',row)
-        continue
-    station_ID = recover_integer(station.station_ID)
-    if (station_ID == -1):
-        print('no station saved with name ',row["Station"])
-        continue
-    no2_value = recover_integer(row['NO2 Value'])
-    o3_value = recover_integer(row['O3 Value'])
-    pm10_value = recover_integer(row['PM10 Value'])
-    date_generated = recover_date(str(row.Generated))
-    row_values = [no2_value, o3_value, pm10_value, date_generated, int(station_ID)]
-    db_conn.insert_values(table_name, column_names, values, row_values)
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/air-quality-nov-2017.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_06_Juny_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder, sep=';')
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_08_Agost_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_09_Setembre_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_10_Octubre_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_11_Novembre_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2018_12_Desembre_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2019_01_Gener_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+data_folder = Path('./Project-Week-2-Barcelona/datasets/2.-Urban-Environment/2019_02_Febrer_qualitat_aire_BCN.csv')
+registry_df = pd.read_csv(data_folder)
+print('\nimported data -->\n', registry_df)
+run_csv_file_into_sql(registry_df, stations)
+
+
+
+
+
 
 # close connections
 db_conn.close_connection()
