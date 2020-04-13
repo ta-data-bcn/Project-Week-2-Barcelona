74a1072c2ef93f1618ad0933ba83def83a02928e
Aitor Quinza <aitor@quinza.es>
2020-04-13 08:45:05 +0200

    Project Finished

--- your-project/Queries/queries.sql
+++ your-project/Queries/queries.sql
@@ -1,32 +1,29 @@
-## Barrios por Distrito
+USE Barrios;
+
+## Neighborhoods by District
 Select NOM_DISTRICTE, count(NOMB_BARRI) as Qty
 FROM Barrios
 GROUP BY NOM_DISTRICTE;
 
-## Cantidad de personas por Barrio
+## Number of people per Neighborhood
 SELECT Distrito, Barrio , sum(Numero) as People
 FROM Population
 GROUP BY Barrio
 ORDER BY People DESC
 LIMIT 10;
 
-## Cantidad de personas desempleadas por Barrio
+## Number of unemployed people per Neighborhood
 SELECT Barrio , sum(Numero) as People
 FROM Unemployment
 GROUP BY Barrio, SItuacion
 ORDER BY People DESC;
 
-## Personas Desempleadas en el 2020
-SELECT * FROM `Unemployment` WHERE `Ano` = '2020' ORDER BY `Numero` LIMIT 0,1000;
 
-##
-SELECT * FROM BusinessTypes WHERE ActivityName LIKE 'Bars%';
-
-## Catidad de Bares
+## Number of Bars
 SELECT * FROM CensoActividades
 WHERE NombreActividad LIKE 'Bars%';
 
-## Cantidad de Bares por Barrio
+## Number of Bars, Restaurants and Food Services per Neighborhood
 SELECT Distrito ,Barrio, count(NombreLocal) as Companies
 FROM CensoActividades
 WHERE NombreActividad LIKE 'Bars%' or NombreActividad = 'serveis de menjar i begudes' or NombreActividad = 'Restaurants' or NombreActividad = 'Serveis de menjar take away MENJAR RÀPID' or NombreActividad = 'Bars especials amb actuació / Bars musicals / Discoteques /PUB'
@@ -34,15 +31,8 @@ group by Barrio
 ORDER BY Companies DESC
 LIMIT 10;
 
-SELECT Barrio, count(NombreLocal) as Companies
-FROM CensoActividades
-WHERE NombreActividad LIKE 'Bars%' or NombreActividad = 'serveis de menjar i begudes' or NombreActividad = 'Restaurants' or NombreActividad = 'Serveis de menjar take away MENJAR RÀPID' or NombreActividad = 'Bars especials amb actuació / Bars musicals / Discoteques /PUB'
-group by Barrio
-ORDER BY Companies ASC
-LIMIT 10;
-
-## Top 10 barrios con menos desempleo
-SELECT Barrio , sum(Numero) as Unemployed, sum(P.Numero)
+## Top 10 neighborhoods with less unemployment
+SELECT Unemployment.Barrio , sum(P.Numero) as Unemployed
 FROM Unemployment
 JOIN Barrios B on Unemployment.Barrio = B.NOMB_BARRI
 JOIN Population P on B.NOMB_BARRI = P.Barrio
@@ -50,15 +40,36 @@ GROUP BY Barrio, SItuacion
 ORDER BY Unemployed ASC
 LIMIT 10;
 
-## Calcular Rango Edad de las personas por Barrios
+## Calculate age range per Neighborhood
 SELECT Year, AgeRange,Barrio , sum(Numero) as People
 FROM Population
 GROUP BY AgeRange, Year, Barrio
 ORDER BY Year DESC;
 
+## Calculate Age Range of People by Neighborhood
 SELECT Distrito, Barrio, AVG(EdadMedia) as EdadMedia
 FROM PoblacionPorEdadesYNac
 GROUP BY Barrio
 ORDER BY EdadMedia ASC;
 
+## Unemployment Rate (Not Working)
+SELECT Population.Barrio, SUM(Population.Numero) as People, SUM(U.Numero), U.Gender, (SUM(U.Numero)/SUM(Population.Numero)*100) as Rate
+FROM Population
+INNER JOIN Unemployment U on Population.Barrio = U.Barrio and U.SItuacion = "Atur registrat"
+GROUP BY Population.Barrio,U.Gender
+ORDER BY People DESC
+LIMIT 12;
 
+## price directly on those with the lowest average age
+SELECT PrecioLocalesComerciales.Barrio, PrecioLocalesComerciales.Precio2011, PrecioLocalesComerciales.Precio2010, PrecioLocalesComerciales.Precio2009, AVG(PPEYN.EdadMedia) AS Average
+FROM PrecioLocalesComerciales
+INNER JOIN PoblacionPorEdadesYNac PPEYN on PrecioLocalesComerciales.Barrio = PPEYN.Barrio
+GROUP BY PrecioLocalesComerciales.Barrio
+ORDER BY Average ASC;
+
+
+SELECT PrecioLocalesComerciales.Barrio, PrecioLocalesComerciales.Precio2011, PrecioLocalesComerciales.Precio2010, PrecioLocalesComerciales.Precio2009, AVG(PPEYN.EdadMedia) AS Average
+FROM PrecioLocalesComerciales
+    INNER JOIN PoblacionPorEdadesYNac PPEYN on PrecioLocalesComerciales.Barrio = PPEYN.Barrio
+GROUP BY PrecioLocalesComerciales.Barrio
+ORDER BY Average ASC;
